d1ac97da60eb0f4ae39f193c4c243d81248ebea8
Abby <abby_bush1@baylor.edu>
2023-01-30 14:10:48 -0600

    class on monday january 30

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,26 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+
+print(person)
+
+# The name of the second child
+
+print(person["children"][1])
+
+# The name of the cat
+
+print(person["pets"]["cat"])
+
+
+# use a for loop to list each child
+
+for i in person["children"]:
+    print(i)
+
+# print out the pets in this format;
+#'Type of pet:dog name of pet:Fido'
+
+for i, j in person["pets"].items():
+    print(f"Type of pet: {i} name of pet: {j}")
